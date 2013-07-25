9f89012c705c371839685426b829e6e5f5212093
Arsen Miemietov <arsen@exss.org>
2013-07-25 17:19:22 +0300

    support version display

---
+++ scripts/_Events.groovy
@@ -0,0 +1,22 @@
+import java.text.SimpleDateFormat
+
+eventCompileStart = { kind ->
+	def buildNumber = metadata.'app.buildNumber'
+ 
+	if (!buildNumber)
+		buildNumber = 1
+	else
+		buildNumber = Integer.valueOf(buildNumber) + 1
+ 
+	metadata.'app.buildNumber' = buildNumber.toString()
+ 
+	def formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm")
+	def buildDate = formatter.format(new Date(System.currentTimeMillis()))
+	metadata.'app.buildDate' = buildDate
+	metadata.'app.buildProfile' = grailsEnv
+	
+	
+	metadata.persist()
+ 
+	println "**** Compile Starting on Build #${buildNumber}"
+}
