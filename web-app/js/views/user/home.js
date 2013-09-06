60181e01f7aa9b01780c7396e01d65eea57a657f
murmaeuschen <chumako@gmail.com>
2013-09-06 12:16:40 +0300

    homepage

--- web-app/js/views/user/home.js
+++ web-app/js/views/user/home.js
@@ -5,8 +5,9 @@ App.Views.Home = Backbone.View.extend({
 		var hospital = this.model.get('name');
 		var pr_ehr = this.model.get('ehr').code;
 		var add_ehrs = this.model.get('externalEHRs');
+		add_ehrs = add_ehrs.replace(/\n/g, ", ");		
 		var codes = _.pluck(this.model.get('products'), 'code');				
-		products = codes.join(',');		
+		products = codes.join(', ');		
 		var pop_methode = this.model.get('populationMethod');
 		
 		this.$el.html(this.template({hospital:hospital, pr_ehr:pr_ehr, add_ehrs:add_ehrs, products:products, pop_methode: pop_methode}));		
