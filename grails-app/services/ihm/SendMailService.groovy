d7cf64efbd94358f923df87192dddc2161a63e7c
Bogdan <btodor@softserveinc.com>
2013-09-26 13:16:55 +0300

    task 179

--- grails-app/services/ihm/SendMailService.groovy
+++ grails-app/services/ihm/SendMailService.groovy
@@ -1,9 +1,19 @@
 package ihm
 
+import grails.plugin.asyncmail.AsynchronousMailService
+
 class SendMailService {
 	
-	def mailService
 	def grailsApplication
+	AsynchronousMailService asyncMailService
+	
+	private void sendMail(String sendTo, String subjectM, String bodyM){
+		asyncMailService.sendMail {
+		    to transferToList(sendTo).toArray()
+		    subject "$subjectM"
+			body "$bodyM"
+		}
+	}
 	
 	
 	private ArrayList transferToList(String emails){
@@ -15,83 +25,43 @@ class SendMailService {
 	}
 	
 	def assignProductToHospital(String sendTo, String hospitalName, String productName, Date updateDate) {
-		mailService.sendMail {
-			to transferToList(sendTo).toArray()
-			subject "New Product"
-			body "The $productName product has been assigned to $hospitalName on $updateDate."
-		}
+		sendMail ( sendTo, "New Product", "The $productName product has been assigned to $hospitalName on $updateDate.")
     }
 	
 	def deAssignProductFromHospital(String sendTo, String hospitalName, String productName, Date updateDate) {
-		mailService.sendMail {
-			to transferToList(sendTo).toArray()
-			subject "Product Has Been Removed"
-			body "The $productName product has been removed from $hospitalName on $updateDate."
-		}
+		sendMail (sendTo, "Product Has Been Removed", "The $productName product has been removed from $hospitalName on $updateDate.")
 	}	
 	
 	def updateHospitalConfig (String sendTo, String hospitalName, Date updateDate) {
-		mailService.sendMail {
-			to transferToList(sendTo).toArray()
-			subject "Hospital Configuration Has Been Updated"
-			body "The hospital configuration for $hospitalName has been updated  on $updateDate"
-		}
+		sendMail(sendTo,"Hospital Configuration Has Been Updated","The hospital configuration for $hospitalName has been updated  on $updateDate")
 	}
 	
 	def includeMeasureIntoHospitalProduct (String sendTo, String hospitalName, String productName, String measureName, Date updateDate) {
-		mailService.sendMail {
-			to transferToList(sendTo).toArray()
-			subject "Measure added"
-			body "The $measureName measure has been added to the $productName product for  $hospitalName on $updateDate."
-		}
+		sendMail (sendTo, "Measure added", "The $measureName measure has been added to the $productName product for  $hospitalName on $updateDate.")
 	}
 	
 	def markMeasureAsComplete (String sendTo, String hospitalName, String productName, String measureName, Date updateDate, String userName) {
-		mailService.sendMail {
-			to transferToList(sendTo).toArray()
-			subject "Measure Completed"
-			body "The $measureName measure has been marked complete in the $productName product for $hospitalName on $updateDate by $userName."
-		}
+		sendMail (sendTo, "Measure Completed", "The $measureName measure has been marked complete in the $productName product for $hospitalName on $updateDate by $userName.")
 	}
 	
 	def asseptMeasureThatCompleted (String sendTo, String hospitalName, String productName, String measureName, Date updateDate, String userName) {
-		mailService.sendMail {
-			to transferToList(sendTo).toArray()
-			subject "Measure Accepted"
-			body "The $measureName measure has been marked accepted in the $productName product for $hospitalName on $updateDate by $userName.  Once the data has been verified you will receive additional notification."
-		}
+		sendMail (sendTo, "Measure Accepted", "The $measureName measure has been marked accepted in the $productName product for $hospitalName on $updateDate by $userName.  Once the data has been verified you will receive additional notification.")
 	}
 	
 	def omissionUserIdentifie (String sendTo, String hospitalName, String productName, String measureName) {
-		mailService.sendMail {
-			to transferToList(sendTo).toArray()
-			subject "Measure Needs Review"
-			body "IHM has determined that the $measureName measure in the $productName product for $hospitalName needs additional review.  Please review and update the measure based on IHM comments."
-		}
+		sendMail (sendTo, "Measure Needs Review", "IHM has determined that the $measureName measure in the $productName product for $hospitalName needs additional review.  Please review and update the measure based on IHM comments.")
 	}
 	
 	def verifieMeasure (String sendTo, String hospitalName, String productName, String measureName, Date updateDate, String userName) {
-		mailService.sendMail {
-			to transferToList(sendTo).toArray()
-			subject "Measure Verified"
-			body "The $measureName measure has been marked verified in the $productName product for $hospitalName on $updateDate by $userName."
-		}
+		sendMail (sendTo, "Measure Verified", "The $measureName measure has been marked verified in the $productName product for $hospitalName on $updateDate by $userName.")
 	}
 	
 	def updateDataElement (String sendTo, String hospitalName, String dataElement, String measureName, Date updateDate, String userName) {
-		mailService.sendMail {
-			to transferToList(sendTo).toArray()
-			subject "Data Element Has Been Updated"
-			body "$userName has updated the $dataElement data element within $measureName for $hospitalName on $updateDate."
-		}
+		sendMail (sendTo, "Data Element Has Been Updated", "$userName has updated the $dataElement data element within $measureName for $hospitalName on $updateDate.")
 	}
 	
 	def attachFile (String sendTo, String hospitalName, String dataElement, String measureName, Date updateDate, String userName) {
-		mailService.sendMail {
-			to transferToList(sendTo).toArray() 
-			subject "File Attached to Data Element"
-			body "$userName has attached a file for the $dataElement data element within $measureName for $hospitalName on $updateDate."
-		}
+		sendMail (sendTo, "File Attached to Data Element", "$userName has attached a file for the $dataElement data element within $measureName for $hospitalName on $updateDate.")
 	}
 	
 }
