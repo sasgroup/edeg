6373ecf7c27cc98156151451b481f7514dab4854
murmaeuschen <chumako@gmail.com>
2013-09-27 17:56:23 +0300

    remove last comma in EHRS

--- web-app/js/views/user/home.js
+++ web-app/js/views/user/home.js
@@ -6,7 +6,14 @@ App.Views.Home = Backbone.View.extend({
 		var hospital = this.model.get('name');
 		var pr_ehr = this.model.get('ehr').code;
 		var add_ehrs = this.model.get('externalEHRs');
-		add_ehrs = add_ehrs.replace(/\n/g, ", ");		
+		add_ehrs = add_ehrs.replace(/\n/g, ", ");
+		
+		add_ehrs = add_ehrs.replace(/^\s+|\s+$/g,'');
+		
+		if (add_ehrs.substr(-1, 1) == ',')	{
+			add_ehrs = add_ehrs.substr(0, add_ehrs.length-1);
+		}
+		
 		var codes = _.pluck(this.model.get('products'), 'code');				
 		products = codes.join(', ');		
 		var pop_methode = this.model.get('populationMethod');
