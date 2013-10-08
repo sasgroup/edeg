4741578f4aa63401dbe5d05f66829ec9b578ede7
Bogdan <btodor@softserveinc.com>
2013-10-08 14:12:38 +0300

    crud ValuesType

---
+++ grails-app/controllers/ihm_demo/ValuesTypeController.groovy
@@ -0,0 +1,85 @@
+package ihm_demo
+
+class ValuesTypeController {
+
+   private ValuesType saveInstance (ValuesType instance, def param) {
+		instance.name = param.name
+		instance.description = param.description
+		instance.save(flush :true)
+		
+		return instance
+	}
+	
+	def save() {
+		def valuesType = saveInstance (new ValuesType(), params)
+		render(contentType: "text/json") {
+			resp = "ok"
+			message = "EHR ${valuesType.code} has been successfully created"
+		}
+	}
+   
+	def show() {
+		if (params.id && ValuesType.exists(params.id)) {
+			def result = ValuesType.get(params.id)
+
+			render(contentType: "text/json") {
+				version = result.version
+				name =result.name
+				notes=result.description
+				id   = result.id	
+			}
+		}
+		else {
+			def results = ValuesType.list()
+			
+			render(contentType: "text/json") {
+				valuesType = array {
+					for (v in results) {
+						valuesType name: v.name, description: v.description, id: v.id
+					}
+				}
+			}
+		}
+	}
+
+	
+	def update(Long id, Long version) {
+		def valuesType = ValuesType.get(id)
+
+		if  (!valuesType) {
+			render(contentType: "text/json") {
+				resp = "error"
+				message = "Id exceptions"
+			}
+		}
+
+		 if (params.version != null) {
+            if (ehr.version > params.version) {
+				return render(contentType: "text/json") {
+					resp = "error"
+					message = "Another user edited this record and saved the changes before you attempted to save your changes. Re-edit the record ${valuesType}."
+				}
+			} 
+		 }	
+		
+		valuesType  = saveInstance(valuesType, params)
+		render(contentType: "text/json") {
+			resp = "ok"
+			message = "EHR ${valuesType} has been successfully updated"
+		}
+	}
+	
+
+	def delete(Long id) {
+		def valuesType = ValuesType.findById(id)
+		
+		
+			valuesType?.delete(flush: true)
+			render(contentType: "text/json") {
+				resp = "success"
+				message = "ValuesType ${valuesType} has been successfully deleted"
+			}
+		
+	}
+}
+
