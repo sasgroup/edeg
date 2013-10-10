99540b389a88dc5c8a77dd0191acb851341bc3ac
Bogdan <btodor@softserveinc.com>
2013-10-10 12:27:50 +0300

    ids in DataElementControllers

--- grails-app/controllers/ihm_demo/DataElementController.groovy
+++ grails-app/controllers/ihm_demo/DataElementController.groovy
@@ -24,7 +24,7 @@ class DataElementController {
 		def dataElementsDefaults = param?.dataElementDefaults
 		for (dataElementsDefault in dataElementsDefaults) {
 			if (dataElementsDefault.location)
-				new DataElementDefaults(location:dataElementsDefault.location, valueType:dataElementsDefault.valueType.name,dataElement : instance, ehr : Ehr.get(dataElementsDefault.linkId), ids : "").save(flush:true)
+				new DataElementDefaults(location:dataElementsDefault.location, valueType:dataElementsDefault.valueType.name,dataElement : instance, ehr : Ehr.get(dataElementsDefault.linkId), ids : dataElementsDefault.ids).save(flush:true)
 		}
 
 		return instance
@@ -59,7 +59,8 @@ class DataElementController {
 						dataElementDefault	id : d.id,
 						location : d.location,
 						valueType : d.valueType,
-						linkId : d.ehr.id
+						linkId : d.ehr.id,
+						ids : d.ids
 					}
 				}
 			}
