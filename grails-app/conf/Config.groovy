98cd82b86a333880fd9ce5bed4fa139f2aa69f01
Bogdan <btodor@softserveinc.com>
2013-11-07 10:47:22 +0200

    move Config files external to App

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -1,193 +1,32 @@
-// locations to search for config files that get merged into the main config;
-// config files can be ConfigSlurper scripts, Java properties files, or classes
-// in the classpath in ConfigSlurper format
 
-// grails.config.locations = [ "classpath:${appName}-config.properties",
-//                             "classpath:${appName}-config.groovy",
-//                             "file:${userHome}/.grails/${appName}-config.properties",
-//                             "file:${userHome}/.grails/${appName}-config.groovy"]
-
-// if (System.properties["${appName}.config.location"]) {
-//    grails.config.locations << "file:" + System.properties["${appName}.config.location"]
-// }
-
-grails.project.groupId = appName // change this to alter the default package name and Maven publishing destination
-grails.mime.file.extensions = true // enables the parsing of file extensions from URLs into the request format
-grails.mime.use.accept.header = false
-grails.mime.types = [
-    all:           '*/*',
-    atom:          'application/atom+xml',
-    css:           'text/css',
-    csv:           'text/csv',
-    form:          'application/x-www-form-urlencoded',
-    html:          ['text/html','application/xhtml+xml'],
-    js:            'text/javascript',
-    json:          ['application/json', 'text/json'],
-    multipartForm: 'multipart/form-data',
-    rss:           'application/rss+xml',
-    text:          'text/plain',
-    xml:           ['text/xml', 'application/xml']
-]
-//audit
-auditLog.verbose = true
-
-auditLog {
-	actorClosure = { request, session ->
-		session.user?.login
-	}
-	TRUNCATE_LENGTH = 4000 //255     
+/**
+ * Running externalized configuration
+ * Assuming the following configuration files
+ * - in root of "C:" Config/[Config.groovy|DataSource.groovy]
+ */
+String separator = File.separator
+println "looking files in: C:${separator}Config${separator}"
+grails.config.locations = []
+def defaultConfigFiles = ["C:${separator}Config${separator}Config.groovy",
+  "C:${separator}Config${separator}DataSource.groovy"]
+defaultConfigFiles.each { filePath ->
+  def f = new File(filePath)
+  if (f.exists()) {
+	grails.config.locations << "file:${filePath}"
+  } else {
   }
-// URL Mapping Cache Max Size, defaults to 5000
-//grails.urlmapping.cache.maxsize = 1000
-
-// What URL patterns should be processed by the resources plugin
-grails.resources.adhoc.patterns = ['/images/*', '/css/*', '/js/*', '/plugins/*']
-
-grails {
-	mail {
-	  host = "smtp.gmail.com"
-	  port = 465
-	  username = "ihm.edeg@gmail.com"
-	  password = "swuzabra"
-	  props = ["mail.smtp.auth":"true",
-			   "mail.smtp.socketFactory.port":"465",
-			   "mail.smtp.socketFactory.class":"javax.net.ssl.SSLSocketFactory",
-			   "mail.smtp.socketFactory.fallback":"false"]
-	}
 }
+String bashSafeEnvAppName = appName.toUpperCase(Locale.ENGLISH).replaceAll(/-/, '_')
 
-// The default codec used to encode data with ${}
-grails.views.default.codec = "none" // none, html, base64
-grails.views.gsp.encoding = "UTF-8"
-grails.converters.encoding = "UTF-8"
-// enable Sitemesh preprocessing of GSP pages
-grails.views.gsp.sitemesh.preprocess = true
-// scaffolding templates configuration
-grails.scaffolding.templates.domainSuffix = 'Instance'
-
-// Set to false to use the new Grails 1.2 JSONBuilder in the render method
-grails.json.legacy.builder = false
-// enabled native2ascii conversion of i18n properties files
-grails.enable.native2ascii = true
-// packages to include in Spring bean scanning
-grails.spring.bean.packages = []
-// whether to disable processing of multi part requests
-grails.web.disable.multipart=false
-
-// request parameters to mask when logging exceptions
-grails.exceptionresolver.params.exclude = ['password']
-
-// configure auto-caching of queries by default (if false you can cache individual queries with 'cache: true')
-grails.hibernate.cache.queries = false
-
-environments {
-    development {
-        grails.logging.jul.usebridge = true
-    }
-    production {
-        grails.logging.jul.usebridge = false
-        // TODO: grails.serverURL = "http://www.changeme.com"
-    }
+def externalConfig = System.getenv("${bashSafeEnvAppName}_CONFIG_LOCATION")
+if (externalConfig) {
+  grails.config.locations << "file:" + externalConfig
 }
-
-// log4j configuration
-log4j = {
-    // Example of changing the log pattern for the default console appender:
-    //
-    //appenders {
-    //    console name:'stdout', layout:pattern(conversionPattern: '%c{2} %m%n')
-    //}
-
-    error  'org.codehaus.groovy.grails.web.servlet',        // controllers
-           'org.codehaus.groovy.grails.web.pages',          // GSP
-           'org.codehaus.groovy.grails.web.sitemesh',       // layouts
-           'org.codehaus.groovy.grails.web.mapping.filter', // URL mapping
-           'org.codehaus.groovy.grails.web.mapping',        // URL mapping
-           'org.codehaus.groovy.grails.commons',            // core / classloading
-           'org.codehaus.groovy.grails.plugins',            // plugins
-           'org.codehaus.groovy.grails.orm.hibernate',      // hibernate integration
-           'org.springframework',
-           'org.hibernate',
-           'net.sf.ehcache.hibernate'
+def externalDataSource = System.getenv("${bashSafeEnvAppName}_DATASOURCE_LOCATION")
+if (externalDataSource) {
+  grails.config.locations << "file:" + externalDataSource
 }
-
-
-
-
-// Added by the JQuery Validation UI plugin:
-jqueryValidationUi {
-	errorClass = 'error'
-	validClass = 'valid'
-	onsubmit = true
-	renderErrorsOnTop = false
-	
-	qTip {
-		packed = true
-	  classes = 'ui-tooltip-red ui-tooltip-shadow ui-tooltip-rounded'  
-	}
-	
-	/*
-	  Grails constraints to JQuery Validation rules mapping for client side validation.
-	  Constraint not found in the ConstraintsMap will trigger remote AJAX validation.
-	*/
-	StringConstraintsMap = [
-		blank:'required', // inverse: blank=false, required=true
-		creditCard:'creditcard',
-		email:'email',
-		inList:'inList',
-		minSize:'minlength',
-		maxSize:'maxlength',
-		size:'rangelength',
-		matches:'matches',
-		notEqual:'notEqual',
-		url:'url',
-		nullable:'required',
-		unique:'unique',
-		validator:'validator'
-	]
-	
-	// Long, Integer, Short, Float, Double, BigInteger, BigDecimal
-	NumberConstraintsMap = [
-		min:'min',
-		max:'max',
-		range:'range',
-		notEqual:'notEqual',
-		nullable:'required',
-		inList:'inList',
-		unique:'unique',
-		validator:'validator'
-	]
-	
-	CollectionConstraintsMap = [
-		minSize:'minlength',
-		maxSize:'maxlength',
-		size:'rangelength',
-		nullable:'required',
-		validator:'validator'
-	]
-	
-	DateConstraintsMap = [
-		min:'minDate',
-		max:'maxDate',
-		range:'rangeDate',
-		notEqual:'notEqual',
-		nullable:'required',
-		inList:'inList',
-		unique:'unique',
-		validator:'validator'
-	]
-	
-	ObjectConstraintsMap = [
-		nullable:'required',
-		validator:'validator'
-	]
-	
-	CustomConstraintsMap = [
-		phone:'true', // International phone number validation
-		phoneUS:'true',
-		alphanumeric:'true',
-		letterswithbasicpunc:'true',
-    lettersonly:'true'
-	]	
+grails.config.locations.each {
+  println "[INFO] Including configuration file [${it}] in configuration building."
 }
 
